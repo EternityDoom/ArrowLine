6b8bbc381c7f7d30604abe57f9f689dca52113c3
EternityDoom <kale@dfddgame.com>
2024-08-30 01:38:23 -0600

    TextMesh Pro setup
    Imported TextMesh Pro and got some of the initial code set up for equations.

---
+++ Assets/Scripts/ArcheryEquation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Notifications.Android;
+using UnityEngine;
+
+public class ArcheryEquation : MonoBehaviour
+{
+    [SerializeField] public int slope;
+    [SerializeField] public int intercept;
+    private EquationPart left, right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EquationPart.cs
@@ -0,0 +1,7 @@
+﻿public interface EquationPart
+{
+    public EquationPart Add(EquationPart other);
+    public EquationPart Subtract(EquationPart other);
+    public EquationPart Multiply(EquationPart other);
+    public EquationPart Divide(EquationPart other);
+}
